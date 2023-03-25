259b70376f12a71844431d1494cfe8fc0d433b7f
sdjothifinance <127538739+sdjothifinance@users.noreply.github.com>
2023-03-25 20:27:47 +0530

    user delete

--- financecenter/src/pages/dashboard/notifications.jsx
+++ financecenter/src/pages/dashboard/notifications.jsx
@@ -42,6 +42,18 @@ export function Notifications() {
   .then(res=>setusers(res.data))
   .catch();
  },[])
+  
+  const deleteuser=(name)=>{
+  const confirmBox = window.confirm(
+    `Do you really want to delete this user(${name})?`
+  )
+  if(confirmBox===true){
+    axios.post("https://impossible-gold-shoulder-pads.cyclic.app/deleteuser",{n:name})
+    .then(res=>console.log(res))
+    .catch()
+  }
+}
+  
 const navigate=useNavigate();
  const passdata=({userId,name,collections})=>{
   return navigate('/dashboard/book',
@@ -70,7 +82,7 @@ const navigate=useNavigate();
           <table className="w-full min-w-[640px] table-auto">
             <thead>
               <tr>
-                {["Id","Name", "Mobile", "Amount", "Installment", "center","Action"].map(
+                {["Id","Name", "Mobile", "Amount", "Installment", "center","Action","Delete"].map(
                   (el) => (
                     <th
                       key={el}
@@ -159,6 +171,11 @@ const navigate=useNavigate();
               View and Pay
             </Button>
                       </td>
+                    <td className={className}>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" onClick={e=>deleteuser(userId)} style={{cursor:'pointer'}}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                      </svg>
+                    </td>
                     </tr>
                   );
                 }
